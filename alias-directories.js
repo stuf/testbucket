86f4410aa8135d927b6faa3a851f542a174ed7ab
Stefan Rimaila <stefan.rimaila@gmail.com>
2017-10-25 11:09:48 +0300

    Update alias-directories.js

--- alias-directories.js
+++ alias-directories.js
@@ -2,16 +2,28 @@ const { readdirSync, lstatSync } = require('fs');
 const { join, resolve } = require('path');
 const R = require('ramda');
 
-module.exports.getDirectories = basePath => {
-  const SRC_ROOT =
-    resolve(basePath, 'src');
+const isDirectory = source => lstatSync(source).isDirectory();
+
+//
+
+module.exports.getJestDirectories = basePath => {
+  const SRC_ROOT = resolve(basePath, 'src');
 
   process.chdir(SRC_ROOT);
-
-  const isDirectory = source =>
-    lstatSync(source).isDirectory();
 
   return readdirSync('.').map(name => join('.', name))
                          .filter(isDirectory)
                          .reduce((obj, n) => R.assoc(`^${n}(.*)$`, `<rootDir>/src/${n}$1`, obj), {});
+};
+
+//
+
+module.exports.getWebpackDirectories = basePath => {
+  const SRC_ROOT = resolve(basePath, 'src');
+
+  process.chdir(SRC_ROOT);
+
+  return readdirSync('.').map(name => join('.', name))
+                         .filter(isDirectory)
+                         .reduce((obj, n) => Object.assign(obj, { [n]: resolve(n) }), {});
 };
